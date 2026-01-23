b3240b5ee8a0fc3be3545c9c3242c0d13dfa27e0
Ali <ali1955.2011@gmail.com>
2026-01-23 16:27:37 +0200

    feat: Add notifications and user schemas with CRUD operations
    - Implemented NotificationCreate, NotificationUpdate, and NotificationOut schemas for managing notifications.
    - Added user-related schemas including UserOut and UserUpdate for user management.
    - Created SQL queries for fetching and manipulating user and notification data.
    - Introduced devtools_options.yaml for Flutter DevTools configuration.

--- Backend/main.py
+++ Backend/main.py
@@ -5,6 +5,8 @@ from routes.users import router as users_router
 from routes.cars import router as cars_router
 from fastapi.middleware.cors import CORSMiddleware
 from routes.car_prices import router as car_prices_router
+from routes.notifications import router as notifications_router
+
 app = FastAPI(title="Car Rental Backend")
 
 
@@ -20,6 +22,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(cars_router)
+app.include_router(notifications_router)
 @app.get("/")
 def root():
     return {"status": "ok"}
